f9d512a065d033f4bbb7f1ac79839ee27b823bdd
Avichay Eyal <eavichay@gmail.com>
2016-06-06 17:42:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 'use strict';
 
 module.exports = {
-
     asUUID: function(buffer) {
         var result = '';
-		for (var i = 0; i < buffer.length; i++) {
-			var chardId;
-			chardId = buffer.readUInt8(i).toString(16);
-			if ([8,12,16,20].indexOf( (i * 2) ) >= 0) {
-				result = result + '-';
-			}
-			result = result + chardId;
-		}
-		return result;
-	},
-	asUINT: function(buffer) {
-	    return buffer.readUInt8(0).toString(10);
-	}
-};
+        for (var i = 0; i < buffer.length; i++) {
+            var chardId;
+            chardId = buffer.readUInt8(i).toString(16);
+            if ([8,12,16,20].indexOf( (i * 2) ) >= 0) {
+                result = result + '-';
+            }
+            result = result + chardId;
+        }
+        return result;
+    },
 
+    asUINT: function(buffer) {
+        return buffer.readUInt8(0).toString(10);
+    }
+};
