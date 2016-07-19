8dd30c8256ac0180682037659dfaf6ffebe273f4
Avichay Eyal <eavichay@gmail.com>
2016-07-19 15:25:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = {
+    
+    /**
+     * @doonce("uuid")
+     */
     asUUID: function(buffer) {
         var result = '';
         for (var i = 0; i < buffer.length; i++) {
